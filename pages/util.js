5a913020e9e7ae70f24ce2a9b1d52101b188d0b0
venuswhispers <venuswhisperer412@gmail.com>
2024-03-21 04:24:26 -0700

    hotfix

--- pages/util.js
+++ pages/util.js
@@ -1,4 +1,4 @@
-export const getUserRating = (solBalance, ethBalance, tokenBalance, tokenValue, solGas, ethGas, followers) => {
+export default getUserRating = (solBalance, ethBalance, tokenBalance, tokenValue, solGas, ethGas, followers) => {
     return Math.round(
         Number(solBalance) + Number(ethBalance) + Number(tokenValue) / Number(tokenBalance) + Number(solGas) + Number(ethGas) + Number(followers)
     )
