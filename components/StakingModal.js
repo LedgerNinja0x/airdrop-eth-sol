7fa3086de60100b80b783d0350b096513f393451
LedgerNinja0x <153813562+LedgerNinja0x@users.noreply.github.com>
2024-05-14 05:55:38 +1000

    fix: backend (#10)

--- components/StakingModal.js
+++ components/StakingModal.js
@@ -81,7 +81,7 @@ export default function StakingModal({
                 onChange={(e) => setPeriod(e.target.value)}
                 placeholder="Input Staking Period"
                 min="0"
-                className="block mb-4 w-3/5 !outline-none rounded-md w-full px-4 border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block mb-4 w-3/5 !outline-none rounded-md px-4 border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
             </div>
             <p>Reward Amount: </p>
--- pages/api/me/message.js
+++ pages/api/me/message.js
@@ -5,36 +5,36 @@ export default async function handler(req, res) {
   try {
     //insert a msg
     if (req.method == 'POST') {
-    await axios.post(
-      `${process.env.MONGODB_URI}/action/updateOne`,
-      {
-        dataSource: "Cluster0",
-        database: process.env.DataBase,
-        collection: "users",
-        filter: {
-          twitt_username: req.body.twitt_username,
-        },
-        update: {
-          $set: {
-            message: {
-              text: process.env.MESSAGE,
-              hashtags: process.env.HASHTAGS.split(","),
+      await axios.post(
+        `${process.env.MONGODB_URI}/action/updateOne`,
+        {
+          dataSource: "Cluster0",
+          database: process.env.DataBase,
+          collection: "users",
+          filter: {
+            twitt_username: req.body.twitt_username,
+          },
+          update: {
+            $set: {
+              message: {
+                text: process.env.MESSAGE,
+                hashtags: process.env.HASHTAGS.split(","),
+              },
             },
           },
         },
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-          apiKey: process.env.DATAAPI_KEY,
-        },
-      }
-    );
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            apiKey: process.env.DATAAPI_KEY,
+          },
+        }
+      );
 
-    console.log('user updated backend')
+      console.log('user updated backend')
 
-    return res.status(201).send("user updated successfully");
+      return res.status(201).send("user updated successfully");
     } else {
       console.log(req.body.username,'username backend')
       //retrieve a msg GET /api/me/message @req.body.username: required
