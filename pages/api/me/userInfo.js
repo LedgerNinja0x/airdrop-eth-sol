0e717e718a128bd2d96895f7f0bae6c8cbc31cdd
trinkstar58 <trinklestar07@gmail.com>
2024-05-21 16:45:45 -0700

    feat: contract-set

--- pages/api/me/userInfo.js
+++ pages/api/me/userInfo.js
@@ -1,22 +1,49 @@
 
 import { getUserRating } from "@/lib/util";
 import TokenAbi from "@/Contracts/erc20.json";
-import contractAddress from "@/Contracts/addresses.json";
 import { providers, utils, ethers } from "ethers";
 import { testRpcProvider } from "@/lib/provider";
 
-
 const getEtherBalance = async (_address) => {
-    for (const item of testRpcProvider) {
-        try {
-            const provider = new providers.JsonRpcProvider(item);
-            const tokenContract = new ethers.Contract(contractAddress.Token, TokenAbi, provider);
-            const tokenWei = await tokenContract.balanceOf(_address);
-            const tokenValue = utils.formatEther(tokenWei);
-            return { tokenValue };
-        } catch (error) {
-            console.log(error);
+    let { data } = await axios.post(
+        `${process.env.MONGODB_URI}/action/find`,
+        {
+          dataSource: "Cluster0",
+          database: process.env.DataBase,
+          collection: "adminData",
+          projection: {},
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            apiKey: process.env.DATAAPI_KEY,
+          },
         }
+    );
+
+    var tokenAddress = "";
+
+    if (data.documents) {
+        const adminData = data.documents[0];
+        tokenAddress = adminData?.tokenAddress ? adminData.tokenAddress : ""
+    }
+
+    if (tokenAddress) {
+        for (const item of testRpcProvider) {
+            try {
+                const provider = new providers.JsonRpcProvider(item);
+                const tokenContract = new ethers.Contract(tokenAddress, TokenAbi, provider);
+                const tokenWei = await tokenContract.balanceOf(_address);
+                const tokenValue = utils.formatEther(tokenWei);
+                return { tokenValue };
+            } catch (error) {
+                console.log(error);
+            }
+        }   
+    } else {
+        const tokenValue = 0;
+        return { tokenValue };
     }
 };
 
