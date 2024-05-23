12dd2a8f73e9fd37d80ac2749d552331782dd368
trinkstar58 <trinklestar07@gmail.com>
2024-05-23 04:13:03 -0700

    fix balance

--- pages/api/me/balance.js
+++ pages/api/me/balance.js
@@ -100,7 +100,6 @@ export default async function handler(req, res) {
             $set: {
               ethAddress,
               ethGas,
-              tokenBalance,
               tokenValue,
               followers_count: followers,
               firstTag
